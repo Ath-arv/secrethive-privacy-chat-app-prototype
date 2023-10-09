cf349489731abe664ef6a6b1c3bf57ceede9f240
Atharv <adasare18@gmail.com>
2023-10-09 15:54:33 +0530

    Db Models added

---
+++ Backend/models/userModel.js
@@ -0,0 +1,13 @@
+ const mongoose = require('mongoose');
+ const UserSchema = mongoose.Schema({
+    name : {type:String , required:true},
+    ip_add:{type:String,required:true},
+    password : {type:String , required:true},
+    picture : {type:String , required:true,
+        default:" https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    }
+
+ },{timestamps : true});
+
+const user = mongoose.model("user",userModel);
+module.exports= user;
