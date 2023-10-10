ddcb713e35d799a74d798cfee8d85b94456366e1
Siddharth Mishra <mishrasiddharth760@gmail.com>
2023-10-10 07:07:29 +0530

    formatted backend files, removed default npm test script, added npm start script, changed server running message, added PORT as a variable

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const { chats } = require("./data/data");
+const PORT = 3000;
 
 const app = express();
-app.get("/api/chat",(req,res)=>{
-
-    res.send(chats);
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
 });
 
-app.get("/api/chat/:id",(req,res)=>{
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
-
-app.listen(3000,()=>{
-    console.log('Active');});
+app.listen(3000, () => {
+  console.log(`Server Running on PORT ${PORT}`);
+});
