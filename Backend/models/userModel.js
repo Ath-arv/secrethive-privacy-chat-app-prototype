a134445462e9e79b024ac78bfd212e41e9e48a74
Atharv <adasare18@gmail.com>
2023-10-21 17:29:35 +0530

    user authentication and user login implemented

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,13 +1,25 @@
  const mongoose = require('mongoose');
+ const bcrypt = require('bcryptjs');
  const UserSchema = mongoose.Schema({
-    name : {type:String , required:true},
+    name : {type:String , required:true,unique:true},
     ip_add:{type:String,required:true},
-    password : {type:String , required:true},
-    picture : {type:String , required:true,
-        default:" https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    password : {type:String , required:true}
+
+ },{timestamps : true}
+ );
+
+ UserSchema.methods.matchPassword = async function(enteredPassword){
+ return await bcrypt.compare(enteredPassword,this.password);
+}
+
+ UserSchema.pre('save',async function(next){   //before saving
+    if(!this.isModified){
+        next();
     }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
 
- },{timestamps : true});
+ });
 
-const user = mongoose.model("user",userModel);
-module.exports= user;
+const User = mongoose.model("User",UserSchema);
+module.exports= User;
